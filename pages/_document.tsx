78a5e790afcbff91c9904a65afc751564bd6b1a7
r-4bb1t <r4bb1t.github@gmail.com>
2023-08-12 09:39:52 +0900

    meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>UVA MEDIA NETWORK</title>
+        <meta property="og:title" content="UVA MEDIA NETWORK" />
+        <meta
+          property="og:image"
+          content="https://uva-media-network.vercel.app/meta.png"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
